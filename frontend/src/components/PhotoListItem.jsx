299b6a7eb3267dcf8c280f0fe620e379cccf7249
Kevin Levasseur <kevin_levasseur@telus.net>
2024-04-22 14:37:18 -0700

    feat: show favorite icon on photo

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -2,9 +2,12 @@ import React from "react";
 
 import "../styles/PhotoListItem.scss";
 
+import PhotoFavButton from "./PhotoFavButton";
+
 const PhotoListItem = (props) => {
   return (
     <div className="photo-list__item">
+      <PhotoFavButton />
       <img className="photo-list__image" src={props.data.imageSource} alt="photograph" />
       <div className="photo-list__user-details">
         <img className="photo-list__user-profile" src={props.data.profile} alt="profile image" />
