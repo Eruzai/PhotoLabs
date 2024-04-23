5c103feac85e6b755e9db21fb7bffec1f9eb5ef1
Kevin Levasseur <kevin_levasseur@telus.net>
2024-04-23 13:40:39 -0700

    feat: render PhotoList instead of PhotoListItems in array

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,16 @@
 import React from 'react';
 
-import PhotoListItem from './components/PhotoListItem';
+import PhotoList from './components/PhotoList';
 import './App.scss';
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
-
-const photos = new Array(3).fill(null);
-const mappedPhotos = photos.map((_, index) => <PhotoListItem key={index} data={sampleDataForPhotoListItem}/>);
+// const photos = new Array(3).fill(null);
+// const mappedPhotos = photos.map((_, index) => <PhotoListItem key={index} data={sampleDataForPhotoListItem}/>);
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   return (
     <div className="App">
-      {mappedPhotos}
+      <PhotoList className="photo-list" />
     </div>
   );
 };
