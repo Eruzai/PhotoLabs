e1873d01d37638ffde54d5b057a3359b54175b5a
Kevin Levasseur <kevin_levasseur@telus.net>
2024-04-24 10:37:27 -0700

    feat: display home route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,12 @@
 import React from 'react';
-
-import TopNavigation from 'components/TopNavigationBar';
-import PhotoList from './components/PhotoList';
+import HomeRoute from 'routes/HomeRoute';
 
 import './App.scss';
 
-// const photos = new Array(3).fill(null);
-// const mappedPhotos = photos.map((_, index) => <PhotoListItem key={index} data={sampleDataForPhotoListItem}/>);
-
-// Note: Rendering a single component to build components in isolation
 const App = () => {
   return (
     <div className="App">
-      <TopNavigation />
-      <PhotoList />
+      <HomeRoute />
     </div>
   );
 };
