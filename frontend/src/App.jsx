99e682693c2044e1e359f50be08782fb639ded54
Kevin Levasseur <kevin_levasseur@telus.net>
2024-04-24 12:50:45 -0700

    chore: remove unneeded line breaks

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,7 @@
 import React from 'react';
-
 import HomeRoute from 'routes/HomeRoute';
-
 import photos from 'mocks/photos';
 import topics from 'mocks/topics';
-
 import './App.scss';
 
 const App = () => {
