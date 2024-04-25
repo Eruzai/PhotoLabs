75112379f4084905c3dd4a142a521fa584395513
Kevin Levasseur <kevin_levasseur@telus.net>
2024-04-25 15:50:25 -0700

    feat: replace useState with useReducer

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,16 +1,42 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
+export const ACTIONS = {
+  FAV_PHOTO_ADDED: 'FAV_PHOTO_ADDED',
+  FAV_PHOTO_REMOVED: 'FAV_PHOTO_REMOVED',
+  SET_TOPIC_DATA: 'SET_TOPIC_DATA',
+  DISPLAY_PHOTO_DETAILS: 'DISPLAY_PHOTO_DETAILS',
+  CLOSE_PHOTO_DETAILS: 'CLOSE_PHOTO_DETAILS'
+};
+
+const INITIAL_STATE = { favorites: [], viewModal: false };
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'FAV_PHOTO_ADDED':
+      return {...state, favorites: [...state.favorites, action.payload]};
+    case 'FAV_PHOTO_REMOVED':
+      return {...state, favorites: state.favorites.filter(id => id !== action.payload)};
+    case 'DISPLAY_PHOTO_DETAILS':
+      return {...state, viewModal: action.payload};
+    case 'CLOSE_PHOTO_DETAILS':
+      return {...state, viewModal: null};
+
+    default:
+      throw new Error(
+        `Tried to reduce with unsupported action type: ${action.type}`
+      );
+    }
+};
 
 const useApplicationData = () => {
-  const [viewModal, setViewModal] = useState(false);
-  const [favorites, setFavorites] = useState([]);
+  const [state, dispatch] = useReducer(reducer, INITIAL_STATE);
 
-  const onPhotoSelect = (data) => setViewModal(data);
-  const onClosePhotoDetailsModal = () => setViewModal(false);
+  const onPhotoSelect = (data) => dispatch({ type: 'DISPLAY_PHOTO_DETAILS', payload: data });
+  const onClosePhotoDetailsModal = () => dispatch({ type: 'CLOSE_PHOTO_DETAILS' });
   
-  const updateToFavPhotoIds = (photoID) => {!favorites.includes(photoID) ? setFavorites([...favorites, photoID]) : setFavorites(favorites.filter(id => id !== photoID))};
+  const updateToFavPhotoIds = (photoID) => dispatch({ type: !state.favorites.includes(photoID) ? 'FAV_PHOTO_ADDED' : 'FAV_PHOTO_REMOVED', payload: photoID });
 
   return {
-    state: {viewModal, favorites},
+    state,
     updateToFavPhotoIds,
     onPhotoSelect,
     onClosePhotoDetailsModal
