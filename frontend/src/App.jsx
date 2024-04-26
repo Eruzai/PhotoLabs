de05b7497ebe2fa815d6037235e9b96f8952cb65
Kevin Levasseur <kevin_levasseur@telus.net>
2024-04-26 13:38:49 -0700

    feat: use state for photos and topics

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import HomeRoute from 'routes/HomeRoute';
 import PhotoDetailsModal from 'routes/PhotoDetailsModal';
 import useApplicationData from 'hooks/useApplicationData';
-import photos from 'mocks/photos';
-import topics from 'mocks/topics';
 import './App.scss';
 
 const App = () => {
@@ -17,7 +15,7 @@ const App = () => {
 
   return (
     <div className="App">
-      <HomeRoute openModal={onPhotoSelect} photos={photos} topics={topics} favorites={state.favorites} toggleFavorite={updateToFavPhotoIds}/>
+      <HomeRoute openModal={onPhotoSelect} photos={state.photoData} topics={state.topicData} favorites={state.favorites} toggleFavorite={updateToFavPhotoIds}/>
       {state.viewModal && <PhotoDetailsModal photoData={state.viewModal} closeModal={onClosePhotoDetailsModal} favorites={state.favorites} toggleFavorite={updateToFavPhotoIds}/>}
     </div>
   );
